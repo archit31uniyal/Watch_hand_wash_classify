85c7d24c3ac70f15fa3557abed1e293ce1db0228
archit31uniyal <archituniyal5@gmail.com>
2024-10-29 18:07:13 -0400

    Updates

--- README.md
+++ README.md
@@ -4,3 +4,25 @@
 - Asus ZenWatch 2
 - Wada.jar for extracting data from watch
 - WeKA for performing classification and statistical analysis
+
+## Instructions
+
+In order to run the files, create a virtual environment using conda and run the following:
+
+```
+pip install -r requirements.txt
+```
+
+The steps for the scripts are as follows:
+
+- Add the csv files in the <mark>raw_data</mark> folder
+- Run the the <mark>convert_csv_to_arff.py</mark> script as follows:
+  
+  ```
+  python convert_csv_to_arff.py --csv_path <path_to_the_csv> --arff_path <path_to_the_arff> 
+  ```
+
+  path_to_the_csv: It is the path to the csv file which needs to be read and converted to arff format
+  path_to_the_arff: It is the path where the arff file would be saved
+
+The script will generate the arff file in the folder <mark>arff_data</mark> folder as well as output the accuracy of the decision tree.
--- convert_csv_to_arff.py
+++ convert_csv_to_arff.py
@@ -6,6 +6,7 @@ import pandas as pd
 import tempfile
 import os
 from compile_data import *
+import argparse
 
 # Read CSV file
 csv_file = '/Users/archit/Documents/Watch_hand_wash_classify/balanced_features.csv'
@@ -14,15 +15,18 @@ csv_file = '/Users/archit/Documents/Watch_hand_wash_classify/balanced_features.c
 arff_file = '/Users/archit/Documents/Watch_hand_wash_classify/arff_data/balanced_features.arff'
 
 class MyWekaUtils:
+
+    def __init__(self, args=None):
+        self.args = args
     
     @staticmethod
-    def classify(arff_path: str, option: int) -> float:
+    def classify(option: int) -> float:
         # Start the JVM if not already running
         if not jvm.started:
             jvm.start()
         
         # Load the ARFF data into Weka Instances
-        data = load_any_file(arff_path, class_index="last")
+        data = load_any_file(args.arff_path, class_index="last")
         # data.class_is_last()
         
         # Select classifier based on the option
@@ -45,20 +49,20 @@ class MyWekaUtils:
         return evaluation.percent_correct
 
     @staticmethod
-    def read_csv(file_path: str) -> pd.DataFrame:
+    def read_csv() -> pd.DataFrame:
         # Read CSV file into a DataFrame
-        data = pd.read_csv(file_path)
+        data = pd.read_csv(args.csv_path)
         if data.empty:
             print("No data found")
             return None
         return data
 
     @staticmethod
-    def csv_to_arff(file_path, df: pd.DataFrame, class_column: str = None) -> str:
+    def csv_to_arff(class_column: str = None) -> str:
         # Save DataFrame as a temporary CSV file
-        df = pd.read_csv(file_path, names = ['mean_x', 'std_x', 'mean_y', 'std_y', 'mean_z', 'std_z', 'Activity'])
+        df = pd.read_csv(args.csv_path, names = ['mean_x', 'std_x', 'mean_y', 'std_y', 'mean_z', 'std_z', 'Activity'])
         # Load the CSV as Weka Instances
-        instances = load_any_file(file_path)
+        instances = load_any_file(args.csv_path)
         
         # If class_column is provided, set it as the class attribute
         if class_column:
@@ -68,22 +72,27 @@ class MyWekaUtils:
             instances.class_index = -1  # No class attribute set; all columns included
         
         # Save Instances as ARFF to a temporary file
-        save_any_file(instances, file_path)
-        print(f"ARFF file '{file_path}' created successfully.")
+        save_any_file(instances, args.csv_path)
+        print(f"ARFF file '{args.csv_path}' created successfully.")
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Hand wash classification using Weka')
+    parser.add_argument('--csv_path', type=str, help='Path to the CSV file', default=csv_file)
+    parser.add_argument('--arff_path', type=str, help='Path to the ARFF file', default=arff_file)
+    args = parser.parse_args()
+
     # Start the JVM
     if not jvm.started:
         jvm.start()
 
 
-    weka_obj = MyWekaUtils()
+    weka_obj = MyWekaUtils(args)
     generate_data()
 
-    df = weka_obj.read_csv(csv_file)
-    weka_obj.csv_to_arff(arff_file, df, class_column='Activity')
-    accuracy = weka_obj.classify(arff_file, 1)
+    df = weka_obj.read_csv()
+    weka_obj.csv_to_arff(class_column='Activity')
+    accuracy = weka_obj.classify(option= 1)
     print(f"Accuracy: {accuracy:.2f}%")
 
     jvm.stop()
